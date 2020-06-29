df49880dd3ffbe588e32730549febaa9f4b27dfa
tjlemperle <tjlemperle@gmail.com>
2020-06-29 01:18:22 -0600

    updating test questions

--- src/components/Student/StudentTest/StudentTest.js
+++ src/components/Student/StudentTest/StudentTest.js
@@ -2,16 +2,20 @@ import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
 
+import './StudentTest.css'
+
 function StudentTest(props) {
 
     const [test, setTest] = useState([])
     const [isLoading, setLoading] = useState(true)
+    const [index, setIndex] = useState(1)
+    const [questionNum, setQuestionNum] = useState(1)
 
     useEffect(() => {
         getTestQuestions()
         // console.log(props.match.params.testid)
     } )
-
+    
     const getTestQuestions = () => {
         axios.get(`/api/test/${props.match.params.testid}`)
         .then(res => {
@@ -20,12 +24,25 @@ function StudentTest(props) {
         })
         .catch(err => console.log(err))
     }
-
-    const mapTestQuestions = () => {
+    
+   
+    
+    const groupBy = (array, key) => {
         
+        return array.reduce((result, currentValue) => {
+           (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue) 
+
+           return result
+        }, {})
     }
 
+    const questions = groupBy(test, 'test_question_id')
+    const questionsLength = Object.keys(questions).length
+
+    console.log(questions)
+    // console.log(questionsLength)
     console.log(test)
+
     return(
         <div>
             <span>Test: </span>
@@ -35,13 +52,16 @@ function StudentTest(props) {
                     <span>Please wait</span>
                 </div>
             :
-                <div>
+                <div className='questions-container'>
                     {test[0].test_name}
-                    {test[0].test_question}
-                    {test[0].test_question_option}
-                    {test[1].test_question_option}
-                    {test[2].test_question_option}
-                    {test[3].test_question_option}
+                    <div>
+                    <span>{questionNum}.</span>
+                    <span>{}</span>
+                    </div>
+                    <div>
+                    {questionNum > 1 ? <button>Previous</button>: null}
+                    {questionNum < questionsLength ? <button>Next</button> : null}
+                    </div>
                 </div>
             }
         </div>
